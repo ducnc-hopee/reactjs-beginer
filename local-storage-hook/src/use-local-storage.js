d9777d994b02181fdd7ca08f9abd7a0a7a187ad0
Joelnwh <joelngwh@gmail.com>
2025-06-06 15:56:17 +0700

    update local-storage-hook

---
+++ local-storage-hook/src/use-local-storage.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+export default function useLocalStorage(keyName, initialValue) {
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(keyName)) || initialValue
+  );
+
+  useEffect(() => {
+    localStorage.setItem(keyName, JSON.stringify(value));
+  }, [value,keyName]);
+  return [value, setValue];
+}
